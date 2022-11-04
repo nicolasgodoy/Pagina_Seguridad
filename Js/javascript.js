521d2ed6868a77e2b611d38b51d5e6ab13a9dcc3
nicolasgodoy <nicolasgodoy540@gmail.com>
2022-11-04 15:08:23 -0300

    Subiendo Pagina Seguridad

---
+++ Js/javascript.js
@@ -0,0 +1,46 @@
+window.sr = ScrollReveal();
+
+    
+
+    sr.reveal('.imagen1', {
+        duration: 2500,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+    
+
+    sr.reveal('.imagen3', {
+        duration: 2500,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    
+    sr.reveal('.p1', {
+        duration: 2000,
+        origin: 'left',
+        distance: '-200px'
+    });
+    sr.reveal('.p2', {
+        duration: 3000,
+        origin: 'left',
+        distance: '-350px'
+    });
+    sr.reveal('.p3', {
+        duration: 4000,
+        origin: 'left',
+        distance: '-400px'
+    });
+    
+    
+    
+    /* PAGINA NOSOTROS */
+
+     sr.reveal('#contenedor-3', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-400px'
+        
+    });
+ 
+    
